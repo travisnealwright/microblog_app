521bedec2c58d5344b933f053d1c7450f3455547
Travis Wright <travis.neal.wright@gmail.com>
2012-10-02 17:24:25 -0600

    microblog_app
    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MicroblogApp::Application.config.secret_token = '5fe993e542ba10d734ab6c6cb1eed81931e6874f46dea54642cda6ba073c2a20aaa4725156e15948f594c84575aa5ac50238c7ecf6c67cf1bd2d8c29a7c5e156'
